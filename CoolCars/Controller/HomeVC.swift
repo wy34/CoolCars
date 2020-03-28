25e06d8d1a0634496b6e6a148f8364155d1cc07e
William Yeung <williamyeung1141998@gmail.com>
2020-03-27 23:45:58 -0400

    Layout template for homevc

--- CoolCars/Controller/HomeVC.swift
+++ CoolCars/Controller/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  CoolCars
 //
 //  Created by William Yeung on 3/27/20.
@@ -8,29 +8,23 @@
 
 import UIKit
 
-class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cars.count
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCarCell {
-            cell.carImage.image = UIImage(named: cars[indexPath.row].image)
-            cell.brandLabel.text = cars[indexPath.row].brand
-            cell.modelLabel.text = cars[indexPath.row].model
-            return cell
-        } else {
-            return UITableViewCell()
-        }
-    }
-    
-    
+    */
+
 }
-
---
+++ CoolCars/Controller/SwapCarVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CoolCars
+//
+//  Created by William Yeung on 3/27/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class SwapCarVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cars.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCarCell {
+            cell.carImage.image = UIImage(named: cars[indexPath.row].image)
+            cell.brandLabel.text = cars[indexPath.row].brand
+            cell.modelLabel.text = cars[indexPath.row].model
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
+}
+
